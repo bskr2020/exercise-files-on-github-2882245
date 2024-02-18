3de24db3e423b0060f3eb5f997d0b3a6d1fe6297
Bhaskar <38725403+bskr2020@users.noreply.github.com>
2024-02-18 18:53:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,8 +47,8 @@ resource "aws_security_group_rule" "blog_http_in" {
 
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
-  from_port   = 80
-  to_port     = 80
+  from_port   = 443
+  to_port     = 443
   protocol    = "tcp"
   cid_blocks  = ["0.0.0.0/0"]
 
